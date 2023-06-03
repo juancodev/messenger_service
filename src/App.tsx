c5f8c571f8de5e31290f4efb16044f92d2fca752
juancodev <montillasanchezjuancarlos@gmail.com>
2023-06-02 23:32:25 -0400

    [⚡ ADD] environment config: -create vite -integration with tailwindcss and postcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts,js,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
